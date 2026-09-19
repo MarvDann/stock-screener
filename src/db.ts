405342d840f02afde1fde2071192a7b37197ca1e
Martin Dann <marvdann76@gmail.com>
2026-09-19 16:36:35 +0100

    create data folder if it doesn't exist

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,11 @@
 import Database from "better-sqlite3";
+import fs from "fs";
 import path from "path";
 
-const DB_PATH = path.join(__dirname, "..", "data", "screener.db");
+const DATA_DIR = path.join(__dirname, "..", "data");
+const DB_PATH = path.join(DATA_DIR, "screener.db");
+
+fs.mkdirSync(DATA_DIR, { recursive: true });
 
 const db = new Database(DB_PATH);
 
